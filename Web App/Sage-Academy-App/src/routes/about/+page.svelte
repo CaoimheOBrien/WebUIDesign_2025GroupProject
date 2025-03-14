655f9d1a9e931000a888b9c8878893dccd540aca
Natalia Ryl <C00294480@setu.ie>
2025-03-14 11:20:15 +0000

    basic about page

--- Web App/Sage-Academy-App/src/routes/about/+page.svelte
+++ Web App/Sage-Academy-App/src/routes/about/+page.svelte
@@ -7,13 +7,54 @@
 </script>
 
 <h1>About Our Team</h1>
-<p>This educational website was created by four students: Natalia Ryl, Caoimhe O'Brien, Aoife Leahy and Lee Lindsay.</p>
+<p>This educational website was created by four students: Natalia, Caoimhe, Aoife and Lee.</p>
 <p>We are Second Year students currently studying Interactive Digital Art and Design in SETU Carlow.</p>
+
 <h2>Introduction</h2>
 
+<div class="OurPics">
+<img src="/natalia.jpg" alt="Natalia Profile">
+<h3>Natalia Ryl</h3>
+<p>Hi!</p>
+</div>
 
+<div class="OurPics">
+    <img src="/caoimhe.jpg" alt="Caoimhe Profile">
+    <h3>Caoimhe O'Brien</h3>
+    <p>Hi!</p>
+</div>
+
+<div class="OurPics">
+    <img src="/aoife.jpg" id="smaller" alt="Aoife Profile">
+    <h3>Aoife Leahy</h3>
+    <p>Hi!</p>
+    </div>
+
+<div class="OurPics">
+    <img src="/lee.jpg" id="smaller" alt="Lee Profile">
+    <h3>Lee Lindsay</h3>
+    <p>Hi!</p>
+</div>
 
 <style>
     h1 { color: #5a9975; }
+
     p { font-size: 18px; }
+
+    h2 {
+    color: #5a9975;
+    }
+
+    img {
+        width: 300px;
+        margin-right: 0.625rem;
+    }
+
+    #smaller {
+        width: 250px;
+    }
+
+    .OurPics{
+        display: inline-block;
+    }
 </style>
