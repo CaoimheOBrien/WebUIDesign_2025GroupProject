26f5bbbcc98b123c775a9361d5aa5002ab80dad3
Caoimhe O'Brien <c00297178@setu.ie>
2025-03-28 12:47:58 +0000

    Started le quizzes

--- Web App/Sage-Academy-App/src/lib/moduleStore.js
+++ Web App/Sage-Academy-App/src/lib/moduleStore.js
@@ -6,19 +6,30 @@ const designModule = {
     topics: [
         {
             name: "Colour Theory",
-            content: "Color theory is the study of how colors work together, how they affect our emotions and perceptions, and how they can be combined to create visually pleasing designs. It can be a valuable tool to not only help us understand more about the world around us but also to create harmonious designs (or decorate your home). Color theory is the art and science of using color. At its core, color is perception. Our eyes see something and the data sent from our eyes to our brains interprets it as a particular color. Objects reflect light in different combinations of wavelengths (radio, gamma rays, and more). Our brains pick up on these wavelength combinations, interpreting them as a phenomenon known as color."
+            content: "content"
         },
         {
-            name: "Gamification",
-            content: "Gamification is the application of game mechanics — like point scoring, timing or competition — to motivate engagement with a product or service. Injecting everyday tasks with a sense of play incentivizes people to not only complete them but to keep coming back for more. Gamification is all about making non-game activities feel like they’re games. While the term “gamification” was coined in 2002, the concept dates back to the 20th century, when retail stores introduced rewards programs to strengthen customer loyalty and industrial managers encouraged factory workers to see themselves in competition with each other."
+            name: "topic1.2",
+            content: "content"
         },
         {
-            name: "Gestalt Principles",
-            content: "Gestalt Principles are principles/laws of human perception that describe how humans group similar elements, recognize patterns and simplify complex images when we perceive objects. Designers use the principles to organize content on websites and other interfaces so it is aesthetically pleasing and easy to understand. Gestalt is German for unified whole. German psychologists Max Wertheimer, Kurt Koffka, and Wolfgang Kohler created the Gestalt Principles in the 1920s. They wanted to understand how people make sense of the confusing things they see and hear. They identified a set of laws that address the natural compulsion to find order in disorder. "
+            name: "topic1.3",
+            content: "content"
         },
         {
-            name: "Dark Patterns",
-            content: "Dark patterns in UX are a design feature that subtly encourages users to perform a specific action. But unlike good UX, dark patterns benefit the company. In some cases, UX designers can take advantage of how users habitually interact with websites and apps to subtly manipulate them into performing certain actions. This is known as dark patterns. Just like good UX, most dark patterns are invisible to users. But unlike good UX, dark patterns benefit the company (rather than the user) by using deception as a tool for conversion. UK–based UX designer Harry Brignull first coined the term in 2010, defining dark patterns as “a user interface that has been carefully crafted to trick users into doing things, such as buying insurance with their purchase or signing up for recurring bills.”"
+            name: "topic1.4",
+            content: "content"
+        }
+    ],
+
+    quizzes: [
+        {
+            name: "Colour Theory Quiz",
+            content: "", /*DO NOT PUT ANYTHING HERE UNDER ANY CIRCUMSTANCES*/ 
+            question1: "Question 1: What colour means angry?",
+            answer1: "Red",
+            answer2: "Blue",
+            answer3: "Orange",
         }
     ]
 };
@@ -30,20 +41,27 @@ const webUIModule = {
 
     topics: [
         {
-            name: "Typography",
-            content: "Typography is an important part of our everyday life. We see it almost everywhere we look. To put it simply, typography is the appearance of text. It’s one of the most important features of every composition, both in the digital and physical worlds. The type choice can make or break every design. It helps to set the mood and emotion of any text and has a huge impact on how the reader perceives it. Typography has been around since the 11th century, however, it existed even before that – as the unique art of creating words in books and magazines without much technology around. Every detail was well-thought, crafted, and carefully carried out.  "
+            name: "topic2.1",
+            content: "content"
         },
         {
-            name: "CSS",
-            content: "Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML (including XML dialects such as SVG, MathML or XHTML). CSS describes how elements should be rendered on screen, on paper, in speech, or on other media. CSS is among the core languages of the open web and is standardized across Web browsers according to W3C specifications. Inline CSS involves applying styles directly to individual HTML elements using the style attribute. Internal or Embedded CSS is defined within the HTML document’s <style> element. It applies styles to specified HTML elements. External CSS contains separate CSS files that contain only style properties with the help of tag attributes (For example class, id, heading, … etc). CSS property is written in a separate file with a .css extension and should be linked to the HTML document using a link tag."
+            name: "topic2.2",
+            content: "content"
         },
         {
-            name: "Grids and Layout",
-            content: "Grids are the backbone of UI design, providing a systematic framework that helps organize and align elements within a layout. By understanding the structure of grids, you can achieve harmonious compositions that enhance visual hierarchy, improve readability, and create a cohesive user experience. Grids consist of intersecting horizontal and vertical lines that form columns and rows, acting as a guide for placing and aligning elements. The number of columns, gutter sizes, and margins can be customized based on the project’s requirements, offering flexibility and creativity in design. The 8-Point Grid system is one of the essential topics when discussing grids and layouts in UI Design. The 8-point grid system is a powerful tool for consistency and alignment in UI design. This system uses multiples of 8 (e.g., 8px, 16px, 24px) for spacing, sizes, and alignments. "
+            name: "topic2.3",
+            content: "content"
         },
         {
-            name: "HTML",
-            content: "Html is an abbreviation. It stands for HyperText Markup Language. HTML determines the structure of web pages. This structure alone is not enough to make a web page look good and interactive. So you'll use assisted technologies such as CSS and JavaScript to make your HTML beautiful and add interactivity, respectively. You can also look at HTML, CSS, and JavaScript this way: HTML is the structure of a house, CSS is the interior and exterior decor, and JavaScript is the electricity, water system, and many other functional features that make the house livable. Since HTML defines the markup for a particular web page, you'll want the text, images, or other embeds to appear in certain ways. For example, you might want some text to be big, other text to be small, and some to be bold, italic, or in bullet point form. HTML has tags that let you get this done. So, there are tags to create headings, paragraphs, bolded words, italicized words, and more."
+            name: "topic2.4",
+            content: "content"
+        }
+    ],
+
+    quizzes: [
+        {
+            name: "Colour Theory Quiz",
+            content: "Question.",
         }
     ]
 };
@@ -55,20 +73,27 @@ const UIProtoModule = {
 
     topics: [
         {
-            name: "Wireframing",
-            content: ""
+            name: "topic3.1",
+            content: "content"
         },
         {
-            name: "Copyright",
-            content: ""
+            name: "topic3.2",
+            content: "content"
         },
         {
-            name: "",
+            name: "topic3.3",
             content: "content"
         },
         {
             name: "topic3.4",
             content: "content"
+        }
+    ],
+
+    quizzes: [
+        {
+            name: "Colour Theory Quiz",
+            content: "Question.",
         }
     ]
 };
@@ -95,6 +120,13 @@ const digitalArtModule = {
             name: "topic4.4",
             content: "content"
         }
+    ],
+
+    quizzes: [
+        {
+            name: "Colour Theory Quiz",
+            content: "Question.",
+        }
     ]
 };
 
@@ -104,4 +136,4 @@ export let modules = [
     UIProtoModule,
     digitalArtModule,
     // rest of modules here
-  ];
+];
--- Web App/Sage-Academy-App/src/routes/student_quiz_home_page/+page.svelte
+++ Web App/Sage-Academy-App/src/routes/student_quiz_home_page/+page.svelte
@@ -1,106 +1,215 @@
 <script>
-    import { onMount } from 'svelte';
+  import { onMount } from 'svelte';
+  import { modules } from '$lib/moduleStore.js';
 
-    onMount(() => {
-        console.log("Staff Home Page Loaded");
-    });
-    
-    let selectedModule = null;
-    let selectedTopic = null;
+  onMount(() => {
+      console.log("Student Quizzes Home Page Loaded");
+  });
 
-    function selectModule(module) {
-    selectedModule = module;
-    selectedTopic = null;
+  /**
+	 * @type {{ name: any; quizzes: any; } | null}
+	 */
+  let selectedModule = null;
+
+  /**
+	 * @type {{ name: any; content: any; } | null}
+	 */
+  let selectedQuiz = null;
+
+  /**
+	 * @param {{ name: any; quizzes: any; } | null} module
+	 */
+  function selectModule(module) {
+  selectedModule = module;
+  selectedQuiz = null;
+}
+
+/**
+	 * @param {{ name: any; content: any; } | null} quiz
+	 */
+function selectQuiz(quizzes) {
+      selectedQuiz = quizzes;
   }
 
-  function selectTopic(topic) {
-        selectedTopic = topic;
-    }
+  function goBack() {
+      selectedQuiz = null;
+  }
 
-    function goBack() {
-        selectedTopic = null;
-    }
+  function goBackPage() {
+      window.history.back();
+  } 
+
 </script>
 
+<div class="container">
 <button on:click={goBackPage} class="page-back-btn">← Back</button>
-
 <nav>
     <ul>
-        <li>
-            <img src="brain.png" alt=""/>
-            Design Psych.
-
+      {#each modules as module}
+        <li on:click={() => selectModule(module)}>
+          <img src={module.image} alt={module.alt}/>
+          {module.name}
         </li>
-        <li>
-            <img src="computer.png" alt=""/>
-            Web UI Design
-        </li>
-        <li>
-            <img src="prototype.png" alt=""/>
-            UI Prototyping
-            
-        </li>
-        <li>
-            <img src="art.webp" alt=""/>
-            3D Digital Art
-        </li>
+      {/each}
     </ul>
 </nav>
 
+
+{#if selectedModule}
+<div class="quizzes">
+  {#if selectedQuiz}
+    <button on:click={goBack} class="back-btn">← Back</button>
+    <h1>{selectedQuiz.name}</h1>
+
+    <p>{selectedQuiz.question1}</p>
+    <div class= "answers">
+      <p>{selectedQuiz.answer1}</p>
+      <p>{selectedQuiz.answer2}</p>
+      <p>{selectedQuiz.answer3}</p>
+    </div> 
+  {:else}
+    <h1>{selectedModule.name}</h1>
+    <ul>
+      {#each selectedModule.quizzes as quiz}
+        <li on:click={() => selectQuiz(quiz)}>{quiz.name}</li>
+      {/each}
+  </ul>
+  {/if}
+</div>
+{/if}
+</div>
+
 <style>
-ul {
-  width: 220px;
-  padding: 0;
-  list-style: none;
+.container {
+display: flex;
+align-items: flex-start;
+gap: 30px;
+padding: 20px;
 }
 
-li  {
-  display: flex;  
-  align-items: center; 
-  gap: 15px;
-  color: black;
-  padding: 15px;
-  margin: 20px 0;
-  font-weight: bold;
-
-  background-color:rgba(129,193,34,255);
-  border: solid #016618 3px;
-
-  background-color:rgba(129,193,34,0.3);
-  border: solid black 2px;
-  border-radius: 10px;
-}
-
-li:hover {
-  background-color: rgba(129, 193, 34, 0.6); 
-  cursor: pointer; 
-  transform: scale(1.05); 
+nav {
+width: 250px;
+flex-shrink: 0;
 }
 
 nav ul {
-  justify-content: center;
-  gap: 10px;
+padding: 0;
+list-style: none;
+}
+
+nav li  {
+display: flex;  
+align-items: center; 
+gap: 15px;
+
+color: white;
+font-weight: bold; 
+padding: 15px;
+margin: 20px 0;
+background-color:rgba(129,193,34,255);
+border: solid #016618 3px;
+
+color: black;
+padding: 15px;
+margin: 10px 0;
+background-color:rgba(129,193,34,0.3);
+border: solid black 2px;
+border-radius: 10px;
+}
+
+nav li:hover {
+background-color: rgba(129, 193, 34, 0.6); 
+cursor: pointer; 
+transform: scale(1.05); 
 }
 
 img {
-    width: 40px; 
-    height: 40px;
+  width: 40px; 
+  height: 40px;
 
-  }
+}
 
-  .page-back-btn {
-    background-color: #016618;
-    color: white;
-    padding: 10px 15px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    font-size: 16px;
-    margin-bottom: 10px;
-  }
+.quizzes {
+  flex-grow: 1; /* Expands to fill space */
+  background-color: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  border: 2px solid black;
+  min-height: 200px;
+}
 
-  .page-back-btn:hover {
-    background-color: #024d13;
-  }
+.quizzes h1 {
+  margin-bottom: 15px;
+}
+
+.quizzes ul {
+  list-style: none;
+  padding: 0;
+}
+
+.quizzes li {
+  padding: 15px;
+  margin: 10px 0;
+  background-color: rgba(129, 193, 34, 0.3);
+  border: solid black 2px;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+}
+
+.quizzes li:hover {
+  background-color: rgba(129, 193, 34, 0.6);
+}
+
+
+.answers {
+  display: inline-flex;
+  padding: 1rem;
+}
+
+.answers p{
+  margin: 1rem; 
+  padding-left: 1.6rem; 
+  padding-right: 1.6rem; 
+  font-size: 2.5rem; 
+
+  background-color: rgba(129, 193, 34, 0.3);
+  border: solid black 2px;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+}
+
+.answers p:hover {
+  background-color: rgba(129, 193, 34, 0.6);
+}
+
+.back-btn {
+  background: none;
+  border: none;
+  color: blue;
+  cursor: pointer;
+  font-size: 16px;
+  margin-bottom: 10px;
+}
+
+.back-btn:hover {
+  text-decoration: underline;
+}
+
+.page-back-btn {
+  background-color: #016618;
+  color: white;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  margin-bottom: 10px;
+}
+
+.page-back-btn:hover {
+  background-color: #024d13;
+}
 
 </style>
