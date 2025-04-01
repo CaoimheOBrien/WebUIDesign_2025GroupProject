59fd6fe546e2c4eb9104a3c47713923cd04e815a
Aoife Leahy <c00296873@setu.ie>
2025-04-01 12:10:34 +0100

    Content of topics completed

--- Web App/Sage-Academy-App/src/lib/moduleStore.js
+++ Web App/Sage-Academy-App/src/lib/moduleStore.js
@@ -6,19 +6,19 @@ const designModule = {
     topics: [
         {
             name: "Colour Theory",
-            content: "content"
+            content: "Color theory is the study of how colors work together, how they affect our emotions and perceptions, and how they can be combined to create visually pleasing designs. It can be a valuable tool to not only help us understand more about the world around us but also to create harmonious designs (or decorate your home). Color theory is the art and science of using color. At its core, color is perception. Our eyes see something and the data sent from our eyes to our brains interprets it as a particular color. Objects reflect light in different combinations of wavelengths (radio, gamma rays, and more). Our brains pick up on these wavelength combinations, interpreting them as a phenomenon known as color."
         },
         {
-            name: "topic1.2",
-            content: "content"
+            name: "Gamification",
+            content: "Gamification is the application of game mechanics — like point scoring, timing or competition — to motivate engagement with a product or service. Injecting everyday tasks with a sense of play incentivizes people to not only complete them but to keep coming back for more. Gamification is all about making non-game activities feel like they’re games. While the term “gamification” was coined in 2002, the concept dates back to the 20th century, when retail stores introduced rewards programs to strengthen customer loyalty and industrial managers encouraged factory workers to see themselves in competition with each other."
         },
         {
-            name: "topic1.3",
-            content: "content"
+            name: "Gestalt Principles",
+            content: "Gestalt Principles are principles/laws of human perception that describe how humans group similar elements, recognize patterns and simplify complex images when we perceive objects. Designers use the principles to organize content on websites and other interfaces so it is aesthetically pleasing and easy to understand. Gestalt is German for unified whole. German psychologists Max Wertheimer, Kurt Koffka, and Wolfgang Kohler created the Gestalt Principles in the 1920s. They wanted to understand how people make sense of the confusing things they see and hear. They identified a set of laws that address the natural compulsion to find order in disorder."
         },
         {
-            name: "topic1.4",
-            content: "content"
+            name: "Dark Patterns",
+            content: "Dark patterns in UX are a design feature that subtly encourages users to perform a specific action. But unlike good UX, dark patterns benefit the company. In some cases, UX designers can take advantage of how users habitually interact with websites and apps to subtly manipulate them into performing certain actions. This is known as dark patterns. Just like good UX, most dark patterns are invisible to users. But unlike good UX, dark patterns benefit the company (rather than the user) by using deception as a tool for conversion. UK–based UX designer Harry Brignull first coined the term in 2010, defining dark patterns as “a user interface that has been carefully crafted to trick users into doing things, such as buying insurance with their purchase or signing up for recurring bills."
         }
     ],
 
@@ -41,20 +41,20 @@ const webUIModule = {
 
     topics: [
         {
-            name: "topic2.1",
-            content: "content"
+            name: "Typography",
+            content: "Typography is an important part of our everyday life. We see it almost everywhere we look. To put it simply, typography is the appearance of text. It’s one of the most important features of every composition, both in the digital and physical worlds. The type choice can make or break every design. It helps to set the mood and emotion of any text and has a huge impact on how the reader perceives it. Typography has been around since the 11th century, however, it existed even before that – as the unique art of creating words in books and magazines without much technology around. Every detail was well-thought, crafted, and carefully carried out. "
         },
         {
-            name: "topic2.2",
-            content: "content"
+            name: "CSS",
+            content: "Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML (including XML dialects such as SVG, MathML or XHTML). CSS describes how elements should be rendered on screen, on paper, in speech, or on other media. CSS is among the core languages of the open web and is standardized across Web browsers according to W3C specifications. Inline CSS involves applying styles directly to individual HTML elements using the style attribute. Internal or Embedded CSS is defined within the HTML document’s <style> element. It applies styles to specified HTML elements. External CSS contains separate CSS files that contain only style properties with the help of tag attributes (For example class, id, heading, … etc). CSS property is written in a separate file with a .css extension and should be linked to the HTML document using a link tag."
         },
         {
-            name: "topic2.3",
-            content: "content"
+            name: "Grids and Layout",
+            content: "Grids are the backbone of UI design, providing a systematic framework that helps organize and align elements within a layout. By understanding the structure of grids, you can achieve harmonious compositions that enhance visual hierarchy, improve readability, and create a cohesive user experience. Grids consist of intersecting horizontal and vertical lines that form columns and rows, acting as a guide for placing and aligning elements. The number of columns, gutter sizes, and margins can be customized based on the project’s requirements, offering flexibility and creativity in design. The 8-Point Grid system is one of the essential topics when discussing grids and layouts in UI Design. The 8-point grid system is a powerful tool for consistency and alignment in UI design. This system uses multiples of 8 (e.g., 8px, 16px, 24px) for spacing, sizes, and alignments."
         },
         {
-            name: "topic2.4",
-            content: "content"
+            name: "HTML",
+            content: "Html is an abbreviation. It stands for HyperText Markup Language. HTML determines the structure of web pages. This structure alone is not enough to make a web page look good and interactive. So you'll use assisted technologies such as CSS and JavaScript to make your HTML beautiful and add interactivity, respectively. You can also look at HTML, CSS, and JavaScript this way: HTML is the structure of a house, CSS is the interior and exterior decor, and JavaScript is the electricity, water system, and many other functional features that make the house livable. Since HTML defines the markup for a particular web page, you'll want the text, images, or other embeds to appear in certain ways. For example, you might want some text to be big, other text to be small, and some to be bold, italic, or in bullet point form. HTML has tags that let you get this done. So, there are tags to create headings, paragraphs, bolded words, italicized words, and more."
         }
     ],
 
@@ -73,20 +73,20 @@ const UIProtoModule = {
 
     topics: [
         {
-            name: "topic3.1",
-            content: "content"
+            name: "Wireframing",
+            content: "A wireframe is a product outline that shows what interface elements will be present on important pages. Low-fidelity wireframes are basic wireframes focused on layout, navigation, and information architecture. They show what the interface will look like, illustrating user flows with key UI design elements. Physical whiteboard sketches can work at early wireframing stages. Mid-fidelity wireframes help designers iterate and shape the final design. This enables teams to settle on the final wireframe design framework before adding visual design components. High-fidelity wireframes look like early product mockups, with interactive and visual design elements—but without the functionality of low-fidelity prototypes. "
         },
         {
-            name: "topic3.2",
-            content: "content"
+            name: "Copyright",
+            content: "Copyright automatically arises when a literary, dramatic, musical or artistic work is created. So, the original UI/UX design elements can be protected by copyright as ‘artistic works’. However, if an alleged copyright infringer makes modifications to the copyrighted design element, he/she can avoid its liability as the protection offered by copyright is limited to copying a substantial portion of the UI/UX design element."
         },
         {
-            name: "topic3.3",
-            content: "content"
+            name: "Accessibility",
+            content: "Accessibility refers to how easily users can interact with products and services to achieve their goals. In simple terms, it measures how effectively users can navigate and use your app or website. While accessibility is often associated with making designs usable for people with disabilities, it actually benefits all users. By prioritizing accessibility in UX/UI design, you can enhance user satisfaction, improve usability, and ensure that your digital products are inclusive and user-friendly for all.There are many different facets of accessibility, such as digital material, physical spaces, transportation, and more."
         },
         {
-            name: "topic3.4",
-            content: "content"
+            name: "Responsive Design",
+            content: "Responsive design is an approach to web design in which the interface adapts to the device's layout to facilitate usability, navigation and information seeking. Responsive design has significant benefits, and it's a default for web design to support device switching. Grid systems are aids designers use to build, design, arrange information and make consistent user experiences. In interaction design, multi-column, hierarchical and modular are the most widely-used types of grids. The principle of a grid is simple: every element occupies the same percentage of space, however large or small the screen becomes, which means that the components can be scaled up and down as the user switches devices."
         }
     ],
 
@@ -99,26 +99,26 @@ const UIProtoModule = {
 };
 
 const digitalArtModule = {
-    name: "3D Digital Art",
+    name: "Digital Art",
     image: "art.webp",
     alt: "art",
 
     topics: [
         {
-            name: "topic4.1",
-            content: "content"
+            name: "Animation",
+            content: "Animation is a filmmaking technique whereby still images are manipulated to create moving images. In traditional animation, images are drawn or painted by hand on transparent celluloid sheets to be photographed and exhibited on film. Many animations are either traditional animations or computer animations made with computer-generated imagery (CGI). Stop motion animation, in particular claymation, has continued to exist alongside these other forms. Computer animation can be very detailed 3D animation, while 2D computer animation (which may have the look of traditional animation) can be used for stylistic reasons, low bandwidth, or faster real-time renderings."
         },
         {
-            name: "topic4.2",
-            content: "content"
+            name: "Character Design",
+            content: "Character art can be all types of genres, from realistic to pixel art, anime styles, or 3D-rendered styles. However, there are some universal aspects to consider when creating a character design, no matter the genre. Silhouette: As well as the body shape and proportions, the type of clothing that your character wears also adds to their portrayal. Color scheme: While a limited color palette isn't required, giving your character a consistent color theme that supports their personality traits can supplement other aspects of their design. Distinctive characteristics: Think about your character's backstory and how you can reflect this on their appearance. Typical expressions: Just as each character has their own distinct personality, the way they express emotions will also be different. "
         },
         {
-            name: "topic4.3",
-            content: "content"
+            name: "3D modeling",
+            content: "In 3D computer graphics, 3D modeling is the process of developing a mathematical coordinate-based representation of a surface of an object (inanimate or living) in three dimensions via specialized software by manipulating edges, vertices, and polygons in a simulated 3D space. Three-dimensional (3D) models represent a physical body using a collection of points in 3D space, connected by various geometric entities such as triangles, lines, curved surfaces, etc. The product is called a 3D model, while someone who works with 3D models may be referred to as a 3D artist or a 3D modeler. A 3D model can also be displayed as a two-dimensional image through a process called 3D rendering or used in a computer simulation of physical phenomena."
         },
         {
-            name: "topic4.4",
-            content: "content"
+            name: "Concept Art",
+            content: "Concept art is a form of visual art used to convey an idea for a final product. It is often created for films, video games, animation, and other forms of media to help visualize the overall look and feel of a project before production begins. Concept art can range from rough sketches to detailed paintings, and it is used to explore different design possibilities and establish a visual direction for a project. It serves as a blueprint for the final product and helps guide the creative process. Concept art plays a crucial role in digital art by helping artists and designers visualize their ideas and communicate them effectively to the rest of the team. It allows for experimentation and iteration, enabling artists to explore different design possibilities and refine their concepts before production begins."
         }
     ],
 
